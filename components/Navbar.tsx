d6c189453992294abcb1eb209ec77fb96c1508a7
shintia zhou <shintiazhouu28@gmail.com>
2022-07-12 11:55:28 +0700

    add icon and logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,27 @@
+import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
+import logo from "../public/logo_full.png";
 
 const Navbar = () => {
   return (
     <nav className="bg-tertiary fixed inset-x-0 top-0 w-full z-20">
-      <div className="container mx-auto flex justify-between items-center py-4 px-10">
-        <section className="flex-1">Search</section>
+      <div className="container mx-auto flex justify-between items-center py-4 md:px-10 px-4">
+        <section className="flex-grow hidden md:block">Search</section>
         <Link href="/">
-          <div className="logo-text">.NextNews</div>
+          <Image
+            objectFit="contain"
+            src={logo}
+            alt="logo"
+            width={130}
+            height={50}
+          ></Image>
         </Link>
-        <section className="flex-1 flex items-center justify-end gap-4">
+        <section className="flex-grow flex items-center justify-end gap-4">
           <Button>Sign In</Button>
-          <Button variant="outlined">Subscribe</Button>
+          <Button className="hidden md:block" variant="outlined">
+            Subscribe
+          </Button>
         </section>
       </div>
     </nav>
